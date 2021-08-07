6e556cfa540fa2ba455248e7610a8c5239038d26
Steve Bao <34757708+stevebao2000@users.noreply.github.com>
2021-08-06 20:01:29 -0700

    add viewModel for MainActivity.kt

---
+++ app/src/main/java/com/steve/droptoken/ui/MainViewModel.kt
@@ -0,0 +1,81 @@
+package com.steve.droptoken.ui
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.steve.droptoken.Constants
+import com.steve.droptoken.Position
+import com.steve.droptoken.R
+import com.steve.droptoken.util.TokenUtil
+import java.lang.IllegalArgumentException
+
+class MainViewModel : ViewModel() {
+    private val TAG = "MainViewModel"
+    val ARRAY_SIZE = Constants.ARRAY_SIZE
+    val tokens : MutableList<Int> = IntArray(ARRAY_SIZE) { Constants.EMPTY_TOKEN }.toMutableList()
+    val tokenImages: Array<Int> = arrayOf(
+        R.drawable.ic_clear,
+        R.drawable.ic_red,
+        R.drawable.ic_blue
+    )
+    var moveSteps: MutableList<Int> = mutableListOf()
+
+    private fun getTokenValue(i: Int, j: Int): Int {
+        return tokens.get(TokenUtil.getIndex(i,j))
+    }
+
+    private fun isTokenEmpty(i: Int, j: Int) : Boolean {
+        return getTokenValue(i,j) == Constants.EMPTY_TOKEN
+    }
+
+    private fun isTokenFilled(i : Int, j: Int): Boolean {
+        return (!isTokenEmpty(i, j))
+    }
+
+    fun isValidPosition(p: Position) : Boolean {
+        return isValidPosition(p.i, p.j)
+    }
+
+    fun isValidIndex(index: Int) : Boolean {
+        if (tokens[index] == Constants.EMPTY_TOKEN) return true
+        return false
+    }
+
+    private fun isValidPosition(a: Int, b: Int) : Boolean {
+        if (isTokenFilled(a, b)) return false
+        if (a == 0) return true
+
+        for (i: Int in 0 until a) {
+            if (isTokenEmpty(i, b))
+                return false
+        }
+        return true
+    }
+
+    fun assignTokenValue(index: Int, value: Int): Boolean {
+        if (!TokenUtil.validValue(value)) return false
+
+        tokens[index] = value
+        moveSteps.add(index)
+        Log.e(TAG, "Now moveSteps: ${moveSteps} " )
+        return true
+    }
+
+    fun resetTokens() {
+        for (i: Int in 0 until ARRAY_SIZE )
+            tokens[i] = Constants.EMPTY_TOKEN
+        moveSteps = mutableListOf()
+    }
+
+    fun getImage(i: Int): Int {
+        if (TokenUtil.validIndex(i))
+            return tokenImages.get(tokens.get(i))
+        throw IllegalArgumentException("Invalid index: $i")
+    }
+
+    fun moveStepsString() : String{
+        return moveSteps.toString()
+    }
+    fun getImageSrc(i: Int) : Int {
+        return tokenImages.get(i)
+    }
+}
