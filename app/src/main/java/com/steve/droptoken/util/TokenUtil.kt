d73ae81ef7e95cd102a89141bae7a106df615e7c
Steve Bao <34757708+stevebao2000@users.noreply.github.com>
2021-08-06 20:04:39 -0700

    move out some methods in to util file

---
+++ app/src/main/java/com/steve/droptoken/util/TokenUtil.kt
@@ -0,0 +1,36 @@
+package com.steve.droptoken.util
+
+import com.steve.droptoken.Constants
+import com.steve.droptoken.Position
+
+object TokenUtil {
+    fun validPosition(i: Int, j: Int) : Boolean {
+        if (i < 0 || i > Constants.MATRIX_SIZE) return false
+        if (j < 0 || j > Constants.MATRIX_SIZE) return false
+        return true
+    }
+
+    fun getIndex(i: Int, j: Int) : Int {
+        return i* Constants.MATRIX_SIZE + j
+    }
+
+//    fun getIndex(p: Position) : Int {
+//        return p.i * Constants.MATRIX_SIZE + p.j
+//    }
+
+    fun validIndex(i: Int) : Boolean {
+        return (i >= 0 && i < Constants.ARRAY_SIZE)
+    }
+
+    fun validValue(value: Int) : Boolean {
+        if (value >= Constants.MIN_VALUE && value <= Constants.MAX_VALUE)
+            return true
+        return false
+    }
+
+    fun getPosition(index: Int): Position {
+        val j: Int = index % Constants.MATRIX_SIZE
+        val i: Int = (index-j) / Constants.MATRIX_SIZE
+        return Position(i,j)
+    }
+}
